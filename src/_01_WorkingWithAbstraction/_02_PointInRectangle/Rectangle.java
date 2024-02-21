8d28d1a814aaf84ce7718f289949864db0c5bf77
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-02-21 12:46:26 +0300

    "finished problem"

---
+++ src/_01_WorkingWithAbstraction/_02_PointInRectangle/Rectangle.java
@@ -0,0 +1,21 @@
+package _01_WorkingWithAbstraction._02_PointInRectangle;
+
+public class Rectangle {
+    private int[] bottomLeftCoordinates = new int[2];
+    private int[] topRightCoordinates = new int[2];
+
+    public Rectangle(String[] coordinates) {
+        bottomLeftCoordinates[0] = Integer.parseInt(coordinates[0]);
+        bottomLeftCoordinates[1] = Integer.parseInt(coordinates[1]);
+        topRightCoordinates[0] = Integer.parseInt(coordinates[2]);
+        topRightCoordinates[1] = Integer.parseInt(coordinates[3]);
+    }
+
+    public boolean contains(Point point) {
+        if ((point.getX() >= bottomLeftCoordinates[0] && point.getX() <= topRightCoordinates[0]) &&
+            (point.getY() >= bottomLeftCoordinates[1] && point.getY() <= topRightCoordinates[1])) {
+            return true;
+        }
+        return false;
+    }
+}
