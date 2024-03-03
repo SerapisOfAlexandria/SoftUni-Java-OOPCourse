a61cc88ef60cef034113880b50095a382b8998fa
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-03 23:11:15 +0300

    "finished problem"

---
+++ src/_05_Inheritance/_03_RandomArrayList/Main.java
@@ -0,0 +1,13 @@
+package _05_Inheritance._03_RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RandomArrayList list = new RandomArrayList();
+        list.getRandomElement();
+    }
+}
