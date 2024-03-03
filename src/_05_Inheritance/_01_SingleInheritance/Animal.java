d2887e477b5f6f28142e5dda6bae2c11913315e3
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-03 22:15:48 +0300

    "finished problem"

---
+++ src/_05_Inheritance/_01_SingleInheritance/Animal.java
@@ -0,0 +1,8 @@
+package _05_Inheritance._01_SingleInheritance;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/_05_Inheritance/_01_SingleInheritance/Dog.java
@@ -0,0 +1,7 @@
+package _05_Inheritance._01_SingleInheritance;
+
+public class Dog extends Animal{
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
