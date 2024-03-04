555f5ba019fc14547d7233acab71f079f4e2cc25
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-04 20:49:21 +0300

    "added solution"

---
+++ src/_06_InheritanceExercise/_06_Animals/animals/Cat.java
@@ -0,0 +1,13 @@
+package _06_InheritanceExercise._06_Animals.animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Meow meow";
+    }
+}
