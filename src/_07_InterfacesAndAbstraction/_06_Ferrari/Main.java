ebd637a0642cec5525c4e5408f2d519d8f42ea00
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-09 22:15:53 +0300

    "added solution"

--- src/_07_InterfacesAndAbstraction/_06_Ferrari/Main.java
+++ src/_07_InterfacesAndAbstraction/_06_Ferrari/Main.java
@@ -2,7 +2,7 @@ package _07_InterfacesAndAbstraction._06_Ferrari;
 
 public class Main {
     public static void main(String[] args) {
-        Car lambo = new Ferrari("Dominick Toretto");
-        System.out.println(lambo.toString());
+        Car ferrari = new Ferrari("Dominick Toretto");
+        System.out.println(ferrari.toString());
     }
 }
