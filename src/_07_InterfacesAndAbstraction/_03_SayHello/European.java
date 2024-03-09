2f980f8342fff886c1b85c1c8218970f7df0e8ca
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-09 18:47:49 +0300

    "added solution"

---
+++ src/_07_InterfacesAndAbstraction/_03_SayHello/European.java
@@ -0,0 +1,20 @@
+package _07_InterfacesAndAbstraction._03_SayHello;
+
+public class European implements Person {
+
+    private String name;
+
+    public European(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
---
+++ src/_07_InterfacesAndAbstraction/_03_SayHello/Person.java
@@ -0,0 +1,8 @@
+package _07_InterfacesAndAbstraction._03_SayHello;
+
+public interface Person {
+
+    String getName();
+
+    String sayHello();
+}
