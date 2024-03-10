4cab0b1465d0ca628f81dea58aecf2c240558658
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-03-10 13:33:40 +0300

    "added solution"

---
+++ src/_08_InterfacesAndAbstractionExercise/_02_MultipleImplementations/Identifiable.java
@@ -0,0 +1,5 @@
+//package _08_InterfacesAndAbstractionExercise._02_MultipleImplementations;
+
+public interface Identifiable {
+    String getId();
+}
---
+++ src/_08_InterfacesAndAbstractionExercise/_02_MultipleImplementations/Person.java
@@ -0,0 +1,6 @@
+//package _08_InterfacesAndAbstractionExercise._02_MultipleImplementations;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
